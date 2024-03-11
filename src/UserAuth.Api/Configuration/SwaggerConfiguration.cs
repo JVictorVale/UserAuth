7bb59d789b0dd5bcc1d8bdd17352189eacc798b9
Victor Vale <joaovictorvale06@gmail.com>
2024-03-11 19:25:58 -0300

    Modificando o contact e lisence no Swagger da api

--- src/UserAuth.Api/Configuration/SwaggerConfiguration.cs
+++ src/UserAuth.Api/Configuration/SwaggerConfiguration.cs
@@ -6,10 +6,30 @@ public static class SwaggerConfiguration
 {
     public static void AddSwagger(this IServiceCollection services)
     {
+        var contact = new OpenApiContact
+        {
+            Name = "Victor Vale",
+            Email = "joaovictorvale.dev@gmail.com\n",
+            Url = new Uri("https://github.com/jvictorvale")
+        };
+        
+        var license = new OpenApiLicense
+        {
+            Name = "Free License",
+            Url = new Uri("https://github.com/jvictorvale")
+        };
+        
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(options =>
         {
             options.EnableAnnotations();
+            options.SwaggerDoc("v1", new OpenApiInfo()
+            {
+                Version = "v1",
+                Title = "Autenticação de Usuario API",
+                Contact = contact,
+                License = license
+            });
 
             options.OrderActionsBy(a => a.GroupName);
             
@@ -20,7 +40,7 @@ public static class SwaggerConfiguration
                 Scheme = "Bearer",
                 BearerFormat = "JWT",
                 In = ParameterLocation.Header,
-                Description = "Insira o token JWT desta maneira: Bearer {seu token}"
+                Description = "JSON Web Token based security\n Insira o token JWT desta maneira: Bearer {seu token}",
             });
             options.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
